a5244d4029ea71bd93bbc20c3068ff3b747d6d38
vitor hernandez <vitorhernandezp7@gmail.com>
2023-03-20 21:34:09 -0300

    primeiro commit

---
+++ src/main/java/br/com/senac/Progweb2Application.java
@@ -0,0 +1,13 @@
+package br.com.senac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Progweb2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Progweb2Application.class, args);
+	}
+
+}
